b4be765c63a777111ef193e81e201ca14fa21ab1
Misael Mercado <misaelmdev@gmail.com>
2018-08-29 20:44:22 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const server = app.listen(4567);
+app.use(express.static('build'));
+console.log('this is working');
+
+const socket = require('socket.io');
+const io = socket(server);
+
+io.sockets.on('connection', newConnection);
+function newConnection(socket) {
+  console.log(`new connection ${socket.id}`);
+  socket.on('mouse', mouseMsg);
+  function mouseMsg(data) {
+    socket.broadcast.emit('mouse', data);
+    console.log(data);
+  }
+
+}
